201b1b12d9e2709059e08f4df97de4e2fc3389c0
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-09-23 19:48:35 +0100

    added login and signup

--- Endpoint/Authentication/User/userController.js
+++ Endpoint/Authentication/User/userController.js
@@ -3,7 +3,7 @@ const {db} = require('../../../db')
 const { sendCookies } = require("../../../utils/setCookies")
 const jwt = require("jsonwebtoken")
 const User = require("../../../Model/users")
-
+const {eq} = require('drizzle-orm')
 //GET All users
 const getusers = async(req,res)=>{
       const users = await db.select().from(User)
@@ -17,15 +17,15 @@ const signupUser = async(req,res)=>{
           if(!email || !password){
                 res.status(400).json({message: "Please fill all fields"})  
             } 
-    const user =await db.select().from(User).where(eq(User.email, email));
-    if(!user){
-    const securePassword = await bcrypt.hash(password, 10)
-    const user = await db.insert(User).values({ email, password: securePassword});
-    res.status(201).json({message: "User Signup Successful"} )
-    }else if(user){
-      res.status(400).json({message:"User already exists"})
-    }
-        } catch (error) {
+            const userExist =await db.select().from(User).where(eq(User.email, email));
+            if(userExist.length <= 0){
+            const securePassword = await bcrypt.hash(password, 10)
+            await db.insert(User).values({ email, password: securePassword});
+            res.status(201).json({message: "User Signup Successful"} )
+            }else if(userExist.length > 0){
+            res.status(400).json({message:"User Already exists"})
+            }
+    } catch (error) {
             console.log(error)
             res.status(400).json({message:error})
         }
@@ -35,14 +35,19 @@ const signupUser = async(req,res)=>{
 const loginUser = async(req,res) =>{
     const {email, password} = req.body
     try {
-        const user =await db.select().from(User).where(eq(User.email, email));
-        if(!user){
+        const user =await db.select().from(User).where(eq(User.email, email)); 
+
+        console.log(user, await bcrypt.compare(password, user[0].password))
+        // const passwordValidateIfUser = user[0]? await bcrypt.compare(password, user[0].password) ?
+        if(user.length <= 0){
             res.status(400).json({message:"Invalid username or Password"})
-        }else if(user && await bcrypt.compare(password, user.password)){
-            const accessToken = await jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "30d"})
+        }else if(user.length > 0 && (await bcrypt.compare(password, user[0].password ) == false)){
+            res.status(400).json({message:"Invalid username or Password"})
+        }
+        else if(user.length > 0 && await bcrypt.compare(password, user[0].password)){
+            const accessToken = await jwt.sign({email}, "secret", {expiresIn: "30d"})
             sendCookies("accessToken", accessToken, res)
             res.status(200).json({message: `Login Successful, welcome`})
-            console.log(accessToken)
         }
     } catch (error) {
         console.log(error)
